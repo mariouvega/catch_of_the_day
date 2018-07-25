4e54aca93cacdfe8f7060b83b8c706e7ece29e04
mariouvega <mario.vega014@gmail.com>
2018-07-24 22:23:29 -0400

    added routing to application with React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-class Header extends React.Component {
-	render() {
-		return (
-			<header className="top">
+// A STF "Stateless Functional Component"
+// Use this when you only pass data via props and retrun some JSX
+// Simplyfied Code Below w/implicit return
+// Always remember to remove this. when calling data, use props.
+const Header = (props) => (
+		<header className="top">
 				<h1>
 					Catch 
 					<span className="ofThe">
@@ -13,11 +15,31 @@ class Header extends React.Component {
 					Day
 				</h1>
 				<h3 className="tagline">
-					<span>{this.props.tagline}</span>
+					<span>{props.tagline}</span>
 				</h3>
 			</header>
-			)
-	}
-}
+	)
 
 export default Header
+
+// This below has been commented out because it was refactored to a Stateless Functional Component
+
+// class Header extends React.Component {
+// 	render() {
+// 		return (
+// 			<header className="top">
+// 				<h1>
+// 					Catch 
+// 					<span className="ofThe">
+// 						<span className="of">Of</span>
+// 						<span className="the">The</span>
+// 					</span>
+// 					Day
+// 				</h1>
+// 				<h3 className="tagline">
+// 					<span>{this.props.tagline}</span>
+// 				</h3>
+// 			</header>
+// 			)
+// 	}
+// }
