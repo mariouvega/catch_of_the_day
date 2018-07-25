1cf65374e36e781f2c13abff6b8471d8de3ff958
mariouvega <mario.vega014@gmail.com>
2018-07-25 16:15:06 -0400

    created the onSubmit event on store picker component form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,15 @@ import { getFunName } from '../helpers';
 
 
 class StorePicker extends React.Component {
+
+	goToStore(e) {
+		e.preventDefault()
+		console.log('hi################')
+	}
+
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter A Store</h2>
 				<input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
 				<button type="submit">Visit Store →</button>
